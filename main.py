d0e2a1430591eb4770b23a7bb9ec77c53eebddfc
Pramith Kiran G B <126304737+Pramith08@users.noreply.github.com>
2024-06-18 18:24:00 +0530

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
 import streamlit as st
+import json
 import time
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post,get
+from googleapiclient.discovery import build
+
+load_dotenv()
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+youtube_api = os.getenv("YOUTUBE_API")
 
 
-youtube_link = st.text_input("Youtube Playlist Link",placeholder="https://www.youtube.com/watch?playlist")
-st.divider()
-st.write("The current movie title is", youtube_link)
+def get_song_names_from_link(playlist_id):
+    song_list=[]
+
+    youtube = build('youtube', 'v3', developerKey=youtube_api)
+
+    # Request playlist items
+    request = youtube.playlistItems().list(
+        part="snippet",
+        playlistId=playlist_id,
+        maxResults=50
+    )
+
+    response = request.execute()
+
+    # Print playlist items and attempt to extract song names
+    for item in response['items']:
+        title = item['snippet']['title']
+
+        if "|" in title:
+            song_name, artist = title.split("|", 1)
+        elif "-" in title:
+            song_name, artist = title.split("|", 1)
+        else:
+            song_name = title
+
+        song_list.append(song_name)
+        
+        print(f'Song Name: {song_name}')
+    return song_list
 
 
-if st.sidebar.button("reset"):
-    st.experimental_rerun()
+### main ###
+
+playlist_id = "PLoxJJjc_pjUFBdHiExeN0ywPJf-aAfFhG"
+
+
+st.write(playlist_id)
---
+++ temp.py
@@ -0,0 +1,67 @@
+
+
+# def get_token():
+#     auth_string = client_id + ":" + client_secret
+#     auth_bytes = auth_string.encode("utf-8")
+#     auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+#     url = "https://accounts.spotify.com/api/token"
+#     headers = {
+#         "Authorization": "Basic " + auth_base64,
+#         "Content-Type": "application/x-www-form-urlencoded",
+#     }
+#     data = {"grant_type": "client_credentials"}
+
+#     result = post(url, headers=headers, data=data)
+#     json_result = json.loads(result.content)
+#     # print(json_result)
+
+#     token = json_result["access_token"]
+#     return token
+
+# def get_auth_header(token):
+#     return {"Authorization": "Bearer " + token}
+
+# def search_for_artist_id(token, artist_name):
+#     search_url = "https://api.spotify.com/v1/search"
+#     headers = get_auth_header(token)
+#     search_query = f"?q={artist_name}&type=artist&limit=1"
+
+#     actual_search_query= search_url + search_query
+
+#     result = get(actual_search_query, headers=headers)
+#     json_result = json.loads(result.content)["artists"]["items"]
+
+#     if len(json_result)==0:
+#         print("No Artist Exist")
+#         return None
+    
+#     artist_id = json_result[0]["id"]
+
+#     return artist_id
+
+# def get_songs_by_artists(token, artist_id):
+#     search_url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks"
+#     headers = get_auth_header(token)
+
+#     result = get(search_url, headers=headers)
+#     json_result = json.loads(result.content)["tracks"]
+
+#     return json_result
+
+    
+
+# token = get_token()
+
+# artist_id = search_for_artist_id(token,"Thaman S")
+
+# songs = get_songs_by_artists(token,artist_id)
+
+# print("token:" + token)
+# print("Artits: ",artist_id)
+
+# for idx,songs in enumerate(songs):
+#     print(f"{idx+1}. {songs['name']}")
+
+
+
